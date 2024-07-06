b75a1d779be86897c2034c30336df6d943442e64
강지우 <jiwookang@JiwooLaptop.local>
2024-07-06 15:30:31 +0900

    front edit

---
+++ teammate_front/lib/tabs/chat.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Tab2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Tab 1',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
